06c6798656b8c7c049e551bcc979a86f7c01cf23
tabi-led <tabiletallecx@gmail.com>
2024-05-15 08:27:29 -0500

    about me piece

---
+++ aboutme.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AboutMe() {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <img src="developer-photo-url" alt="Developer" />
+      <p>Short bio about the developer.</p>
+    </section>
+  );
+}
+
+export default AboutMe;
