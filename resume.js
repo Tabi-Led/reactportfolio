d9268522aa7f053569fa62672bae5f66308488b6
tabi-led <tabiletallecx@gmail.com>
2024-05-15 08:29:53 -0500

    resume commit

---
+++ resume.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Resume() {
+  const proficiencies = ['JavaScript', 'React', 'Node.js', 'CSS', 'HTML'];
+
+  return (
+    <section>
+      <h2>Resume</h2>
+      <a href="resume-link" download>Download Resume</a>
+      <h3>Proficiencies</h3>
+      <ul>
+        {proficiencies.map((skill, index) => <li key={index}>{skill}</li>)}
+      </ul>
+    </section>
+  );
+}
+
+export default Resume;
