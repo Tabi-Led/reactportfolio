3711532eba1350b3be6068f47ec0dbc33e70d68e
tabi-led <tabiletallecx@gmail.com>
2024-05-15 08:30:33 -0500

    footer start

---
+++ footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <a href="https://github.com/developer" target="_blank" rel="noopener noreferrer">GitHub</a>
+      <a href="https://linkedin.com/in/developer" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+    </footer>
+  );
+}
+
+export default Footer;
